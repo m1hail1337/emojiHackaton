7f03b138a32026129ba42b7bc5704af1bcefc420
Zombie1995 <egorkerber1995@mail.ru>
2022-10-02 07:45:14 +0300

    refactoring

--- site/static-file-server.js
+++ site/static-file-server.js
@@ -7,6 +7,14 @@ app.set('port', 8080);
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.post('/site.html', function (req, res) {
+    exec('static_video/static_video_creation.py',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
     res.redirect('back');
 });
 app.listen(app.get('port'), function () {
